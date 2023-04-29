f02b0554085b7d91b9aff929d95d86dbf18cc653
Oliverio <79761509+Oliverio07@users.noreply.github.com>
2023-04-29 13:50:15 -0600

    C# program to print the sum of two numbers.

---
+++ sumOfTwoNumbers/Program.cs
@@ -0,0 +1,24 @@
+﻿// These lines of code write a C# console program to print the sum of two integer numbers.
+int num1 = 2;
+int num2 = 6;
+int sum = num1 + num2;
+Console.WriteLine(sum);
+
+// These lines of code write a C# console program to print the sum of two decimal numbers. 
+decimal num3 = 384_717.8686555m;
+decimal num4 = 400_076m;
+decimal sum2 = num3 + num4;
+Console.WriteLine(sum2);
+
+// These lines of code write a C# console program to print the sum of two double numbers.
+double num5 = 900_000;
+double num6 = 1_000_000;
+double sum3 = num5 + num6;
+Console.WriteLine(sum3);
+
+// These lines of code write a C# console program to print the sum of two float numbers.
+float num7 = 444_444f;
+float num8 = 444_444f;
+Console.WriteLine(num7 + num8);
+
+// I only used sum but I can use any operator I want, such as subtraction, multiplication, division, etc. 
